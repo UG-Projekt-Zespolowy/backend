ad48a5591cedf13bd07c7edbd6102efce62e9289
Damian <d.rogalski.417@studms.ug.edu.pl>
2026-01-04 21:17:08 +0100

    feat: add cors config and auth filter to security config

--- src/main/java/universityproject/taskmanager/config/SecurityConfig.java
+++ src/main/java/universityproject/taskmanager/config/SecurityConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 import universityproject.taskmanager.keycloak.KeycloakUserSyncFilter;
 
 @Configuration
@@ -16,16 +17,18 @@ import universityproject.taskmanager.keycloak.KeycloakUserSyncFilter;
 public class SecurityConfig {
 
     private final KeycloakUserSyncFilter keycloakUserSyncFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/public/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
                 .oauth2ResourceServer(oauth -> oauth.jwt(jwtConfigurer -> {}))
-                .addFilterAfter(keycloakUserSyncFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterAfter(keycloakUserSyncFilter, BearerTokenAuthenticationFilter.class);
         return http.build();
     }
 }
