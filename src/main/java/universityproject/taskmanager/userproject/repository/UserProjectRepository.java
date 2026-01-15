1ec862933c09addebaf2f6e081538fc79424c718
Damian <d.rogalski.417@studms.ug.edu.pl>
2026-01-16 00:26:04 +0100

    feat: change joins to join fetchs

--- src/main/java/universityproject/taskmanager/userproject/repository/UserProjectRepository.java
+++ src/main/java/universityproject/taskmanager/userproject/repository/UserProjectRepository.java
@@ -5,19 +5,27 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import universityproject.taskmanager.userproject.model.UserProject;
 
 @Repository
 public interface UserProjectRepository extends JpaRepository<UserProject, UUID> {
 
-    Page<UserProject> findByProjectId(UUID projectId, Pageable pageable);
+    @Query("SELECT up FROM UserProject up JOIN FETCH up.user JOIN FETCH up.project WHERE up.project.id = :projectId")
+    Page<UserProject> findByProjectId(@Param("projectId") UUID projectId, Pageable pageable);
 
-    Page<UserProject> findByUserId(UUID userId, Pageable pageable);
+    @Query("SELECT up FROM UserProject up JOIN FETCH up.user JOIN FETCH up.project WHERE up.user.id = :userId")
+    Page<UserProject> findByUserId(@Param("userId") UUID userId, Pageable pageable);
 
-    Optional<UserProject> findByUserIdAndProjectId(UUID userId, UUID projectId);
+    @Query(
+            "SELECT up FROM UserProject up JOIN FETCH up.user JOIN FETCH up.project WHERE up.user.id = :userId AND up.project.id = :projectId")
+    Optional<UserProject> findByUserIdAndProjectId(@Param("userId") UUID userId, @Param("projectId") UUID projectId);
 
-    Optional<UserProject> findByProjectIdAndIsOwnerTrue(UUID projectId);
+    @Query(
+            "SELECT up FROM UserProject up JOIN FETCH up.user JOIN FETCH up.project WHERE up.project.id = :projectId AND up.isOwner = true")
+    Optional<UserProject> findByProjectIdAndIsOwnerTrue(@Param("projectId") UUID projectId);
 
     boolean existsByUserIdAndProjectId(UUID userId, UUID projectId);
 
